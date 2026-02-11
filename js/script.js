87f800f52ec95bfa07a7e64e488d165c9ce5d4c3
Faizan4dev <faizan4dev@gmail.com>
2026-02-12 01:41:31 +0500

    Upload interior project

---
+++ js/script.js
@@ -0,0 +1,13 @@
+window.addEventListener('scroll', function() {
+    const reveals = document.querySelectorAll('.reveal');
+
+    reveals.forEach(function(element) {
+        const windowHeight = window.innerHeight;
+        const elementTop = element.getBoundingClientRect().top;
+        const revealPoint = 100;
+
+        if (elementTop < windowHeight - revealPoint) {
+            element.classList.add('active');
+        }
+    });
+});
